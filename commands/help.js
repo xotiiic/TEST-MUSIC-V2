0e13b9dc466161d1844870b6fff757e548dcfee7
xotiiic <143630859+xotiiic@users.noreply.github.com>
2024-06-16 20:06:01 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,8 +36,7 @@ module.exports = {
       .setTitle('💎 Prime Music Bot')
       .setDescription('Welcome to the Music Bot!\n\n- Here are the available commands:\n\n' +
         '**/play :** Start playing the songs.\n' +
-        '**/ping :** check bot latency.\n' +
-        '**/support :** Display support server info');
+        '**/ping :** check bot latency.);
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
