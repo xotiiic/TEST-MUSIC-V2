1e6be36a9748e87803903e06e5789c86d19cfd74
xotiiic <143630859+xotiiic@users.noreply.github.com>
2024-06-16 20:16:31 +0200

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💎 PRIME MUSIC";
-const Text = "GlaceYT";
+const botName = "MUSIC BOT";
+const Text = "Xotiic";
 const version = "Latest@ v1.0"; 
 const startTime = Date.now();
 
