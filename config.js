63a28a8d97642b40ce23e4018999ed23422c93a8
xotiiic <143630859+xotiiic@users.noreply.github.com>
2024-10-19 15:15:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,27 @@ module.exports = {
   ownerID: ["1004206704994566164", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "00fbff",
-  errorLog: "1282293297330782332", 
+  embedColor: "#1db954",
+  musicardTheme:"themes16", //Goes from themes1 to themes19
+  activityName: "Xotiic", // This is bot status Write Anything here 
+  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/QhhMBE2DqE",
+  CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+  MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+  embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
+  errorLog: "", 
 
+   // Lavalink Server Details
+
+  nodes: [
+    {
+        name: "Server",
+        host: "nodev4.fypmoon.org",
+        port: 1118,
+        password: "fypmoon.org",
+        reconnectTimeout: 5000,
+        reconnectTries: Infinity,
+        secure: false
+    },
+ ]
 }
